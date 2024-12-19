c7afce9b21808e0295f37914a5d709515e30692a
ciretorres <erictorres.velasco@gmail.com>
2024-12-19 12:32:41 -0600

    docs: ventajas de p5js sin framework

---
+++ proposals/p5vscode/README.md
@@ -0,0 +1,7 @@
+Ventajas:
+
+- puedo definir las funciones ´setup´ y ´draw´ sin necesidad de crear un objeto que las contega y las llame mediante parámetro
+- importo el cdn de la librería p5js
+- automáticamente el canvas lo coloca dentro de main
+- para webgl no necesito agregarle el this.
+- podría hacer un package para instalar las dependecias
--- proposals/p5vscode/sketch.js
+++ proposals/p5vscode/sketch.js
@@ -1,22 +1,17 @@
 function setup() {
   createCanvas(100, 100, WEBGL);
-
-  describe('A white sphere on a gray background.');
+  describe("A white sphere on a gray background.");
 }
-
 function draw() {
   // background(200);
-  background(0)
-
+  background(0);
   // Enable orbiting with the mouse.
   orbitControl();
-
   // Set the style's sphere.
-  // noStroke();    
-  stroke(255,0,0)
-  fill(255);    
-  // translate(0,0, 0);    
-
+  // noStroke();
+  stroke(255, 0, 0);
+  fill(255);
+  // translate(0,0, 0);
   // Draw the sphere.
   // Set its radius to 30.
   sphere(30);
